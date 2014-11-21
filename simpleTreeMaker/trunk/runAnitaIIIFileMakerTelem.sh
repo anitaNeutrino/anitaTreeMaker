804d0e3a7a031396975ce240abbb193d19d5b416
rjn <rjn@d90a992a-4ffb-0310-9b0e-ce574b03d632>
2014-11-21 22:44:57 +0000

    Fixing detectrion of other monitor files

--- simpleTreeMaker/trunk/runAnitaIIIFileMakerTelem.sh
+++ simpleTreeMaker/trunk/runAnitaIIIFileMakerTelem.sh
@@ -5,10 +5,16 @@ then
    exit 1
 fi
 
+if [ "$ANITA_TELEM_DATA_DIR" = "" ]
+then
+   echo "ANITA_TELEM_DATA_DIR must be set to where the raw_los,openport and tdrss directories are"
+   exit 1
+fi
+
+
 RUN=$1
-BASE_DIR=/anitaStorage/antarctica14
-RAW_RUN_DIR=${BASE_DIR}/telem/raw/run${RUN}
-EVENT_BASE_DIR=${BASE_DIR}/telem/root
+RAW_RUN_DIR=${ANITA_TELEM_DATA_DIR}/raw/run${RUN}
+EVENT_BASE_DIR=${ANITA_TELEM_DATA_DIR}/root
 ROOT_RUN_DIR=${EVENT_BASE_DIR}/run${RUN}
 
 if [[ -d $ROOT_RUN_DIR ]]; then
