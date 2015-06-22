3b398ae9b75eedea42094f2c542278c2461a16f7
strutt <strutt@d90a992a-4ffb-0310-9b0e-ce574b03d632>
2015-06-22 22:11:36 +0000

    Updated for ANITA-3 to loop over end of previous tree to get ring buffer of clock periods to converge.

--- simpleTreeMaker/trunk/makeCalibratedEventTrees.cxx
+++ simpleTreeMaker/trunk/makeCalibratedEventTrees.cxx
@@ -25,42 +25,44 @@
 
 using namespace std;
 //Some global variables
-TTree *fInputHeadTree=0;
-TFile *fInputHeadFile=0;
+// TTree *fInputHeadTree=0;
+// TFile *fInputHeadFile=0;
 TChain *fInputEventChain=0;
+TChain *fInputHeadChain=0;
 RawAnitaHeader *fHeadPtr=0;
 RawAnitaEvent *fRawEventPtr=0;
 CalibratedAnitaEvent *fCalEventPtr=0;
 
 
 char fBaseDir[FILENAME_MAX];
-void loadEventTree(int fCurrentRun);
+char fOutBaseDir[FILENAME_MAX]; // Added fOutBaseDir because I don't have write permission at the moment
+void loadEventTree(int fCurrentRun, Long64_t numPriorEventsToUse);
 void closeEventTree();
 
 int main(int argc, char **argv) {
-  if(argc<3) {
-    std::cout << "Usage: " << basename(argv[0]) << " <base dir>  <run number>" << std::endl;
+  if(argc<4) {
+    std::cerr << "Usage: " << basename(argv[0]) << " <base dir>  <out base dir> <run number>" << std::endl;
     return -1;
   }
   strcpy(fBaseDir,argv[1]);
-  Int_t run=atoi(argv[2]);
+  strcpy(fOutBaseDir,argv[2]);
+  Int_t run=atoi(argv[3]);
   
   char outEventName[FILENAME_MAX];
-  sprintf(outEventName,"%s/run%d/calEventFile%d.root",fBaseDir,run,run);
+  sprintf(outEventName,"%s/run%d/calEventFile%d.root",fOutBaseDir,run,run);
 
  
-  loadEventTree(run);
-  if(fInputEventChain->GetEntries()==0 || !fInputHeadTree) {
+  const Int_t numPriorEvents = 5000;
+  loadEventTree(run, numPriorEvents);
+  if(fInputEventChain->GetEntries()==0 || fInputEventChain->GetEntries()==0) {
     std::cout << "No data for run " << run << "\n";
     return -1;
   }
-
   
   TFile *outEventFile = new TFile(outEventName,"RECREATE");
   TTree* outEventTree = new TTree("eventTree","Tree of Anita Events");
   outEventTree->Branch("run",&run,"run/I");
   outEventTree->Branch("event","CalibratedAnitaEvent",&fCalEventPtr);
-
 
   Long64_t numEvents=fInputEventChain->GetEntries();
 
@@ -74,16 +76,16 @@ int main(int argc, char **argv) {
   for(Long64_t eventEntry=0;eventEntry<numEvents;eventEntry++) {
     if(eventEntry%starEvery == 0) 
       std::cerr << "*";
-      fInputHeadTree->GetEntry(eventEntry);
+      fInputHeadChain->GetEntry(eventEntry);
       fInputEventChain->GetEntry(eventEntry);
       UsefulAnitaEvent realEvent(fRawEventPtr,WaveCalType::kDefault,fHeadPtr);
       if(fCalEventPtr)
 	delete fCalEventPtr;
       fCalEventPtr = new CalibratedAnitaEvent(&realEvent);
       outEventTree->Fill();
-
   }
   outEventTree->AutoSave();
+  outEventFile->Close();
   std::cerr << "\n";
   return 0;
 }
@@ -91,35 +93,101 @@ int main(int argc, char **argv) {
 
 
 
-void loadEventTree(int fCurrentRun)
+void loadEventTree(int fCurrentRun, Long64_t numPriorEventsToUse)
 {       
+
+  // This function has been modified to get some number of events into the RingBuffer 
+  // in AnitaEventCalibrator.
+  // For previous behaviour, pass numPriorEventsToUse=0.
+
   char eventName[FILENAME_MAX];
   char headerName[FILENAME_MAX];
-  sprintf(eventName,"%s/run%d/eventFile%d*.root",fBaseDir,fCurrentRun,fCurrentRun);
-  sprintf(headerName,"%s/run%d/headFile%d.root",fBaseDir,fCurrentRun,fCurrentRun);
+
+  int priorRun = fCurrentRun;
+  Long64_t numPriorEvents = 0;
+
+  fInputEventChain = NULL;
+  fInputHeadChain = NULL;
+
+  std::cout << "numPriorEventsToUse = " << numPriorEventsToUse << std::endl;
+
+  // Here we open prior runs (if they exist) to loop over and work out how many we need,
+  // the answer is probably just one.
+  while(priorRun >= 1 && numPriorEvents < numPriorEventsToUse){
+    priorRun--;
+    sprintf(eventName,"%s/run%d/eventFile%d.root",fBaseDir,priorRun,priorRun);
+    TFile* fPriorEventFile = TFile::Open(eventName);
+    if(fPriorEventFile!=NULL){
+      TTree* fPriorTree = (TTree*) fPriorEventFile->Get("eventTree");
+      numPriorEvents += fPriorTree->GetEntries();
+      std::cout << "priorRun = " << priorRun << "\t";
+      std::cout << "numPriorEvents = " << numPriorEvents << std::endl;
+      fPriorEventFile->Close();
+    }
+  }
+
+  if(numPriorEvents > 0){
+
+    // Create raw event and header chains
+    fInputEventChain = new TChain("eventTree");
+    fInputHeadChain = new TChain("headTree");
+
+    // Add prior runs to those chains
+    for(int run=priorRun; run<fCurrentRun; run++){
+      sprintf(eventName,"%s/run%d/eventFile%d.root",fBaseDir,run,run);
+      fInputEventChain->Add(eventName);
+      fInputEventChain->SetBranchAddress("event",&fRawEventPtr);
+
+      sprintf(headerName,"%s/run%d/headFile%d.root",fBaseDir,run,run);
+      fInputHeadChain->Add(headerName);
+      fInputHeadChain->SetBranchAddress("header",&fHeadPtr);
+    }
+
+    // Get number of events to skip before adding fCurrentRun
+    Long64_t probWayTooManyEventsPrior = fInputEventChain->GetEntries();
+    Long64_t numToSkip = probWayTooManyEventsPrior - numPriorEventsToUse;
+
+    std::cout << "probWayTooManyEventsPrior = " << probWayTooManyEventsPrior << std::endl;
+    std::cout << "numToSkip = " << numToSkip << std::endl;
+
+    // Create the AnitaEventCalibrator in this loop, filling up RingBuffer of clock periods for temp correction
+    for(Long64_t entry=numToSkip; entry<probWayTooManyEventsPrior; entry++){
+      fInputHeadChain->GetEntry(entry);
+      fInputEventChain->GetEntry(entry);
+
+      // Don't do time consuming trigger jitter correction, we only care about temperature correction
+      UsefulAnitaEvent usefulEventToDoTempCorrection(fRawEventPtr, 
+						     WaveCalType::kNoTriggerJitterNoZeroMean, 
+						     fHeadPtr);
+    }
+  }
+
+  std::cout << "Did you create an instance of AnitaEventCalibrator?" << std::endl;
+  std::cout << "If you did, make sure you don't create another!" << std::endl;
+  std::cout << "If you didn't that's OK for run 1." << std::endl;
+
+  // delete the chain we just looped over to fill the RingBuffer in AnitaEventCalibrator
+  // (if we did loop over anything)
+  if(fInputEventChain!=NULL) delete fInputEventChain;
   fInputEventChain = new TChain("eventTree");
+  if(fInputHeadChain!=NULL) delete fInputHeadChain;
+  fInputHeadChain = new TChain("headTree");
+
+  // Add fCurrentRun to our new chain...
+  sprintf(eventName,"%s/run%d/eventFile%d*.root",fBaseDir,fCurrentRun,fCurrentRun);
   fInputEventChain->Add(eventName);
   fInputEventChain->SetBranchAddress("event",&fRawEventPtr);
-
-      
-  fInputHeadFile = new TFile(headerName);
-  if(!fInputHeadFile) {
-    cout << "Couldn't open: " << headerName << "\n";
-    return;
-  }
-  fInputHeadTree = (TTree*) fInputHeadFile->Get("headTree");
-  if(!fInputHeadTree) {
-    cout << "Couldn't get headTree from " << headerName << endl;
-    return;
-  }
-  fInputHeadTree->SetBranchAddress("header",&fHeadPtr);
+  sprintf(headerName,"%s/run%d/headFile%d.root",fBaseDir,fCurrentRun,fCurrentRun);
+  fInputHeadChain->Add(headerName);
+  fInputHeadChain->SetBranchAddress("header",&fHeadPtr);
+  
 }
 
 void closeEventTree() {
-  if(fInputHeadFile) delete fInputHeadFile;
+  if(fInputHeadChain) delete fInputHeadChain;
   if(fInputEventChain) delete fInputEventChain;
-  fInputHeadFile=0;
-  fInputHeadTree=0;
+  fInputEventChain=0;
+  fInputHeadChain=0;
   fHeadPtr=0;
   fInputEventChain=0;
   fRawEventPtr=0; 
