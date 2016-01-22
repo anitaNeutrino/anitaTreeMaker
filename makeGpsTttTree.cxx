ca98cf39b31a0bc8fa2e176c3f7fcfe755ceac10
Ryan Nichol <r.nichol@ucl.ac.uk>
2016-01-22 02:18:26 +0000

    Adding the Ttt Tree maker

---
+++ makeGpsTttTree.cxx
@@ -0,0 +1,101 @@
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <zlib.h>
+#include <libgen.h>   
+
+#include "TTree.h"
+#include "TChain.h"
+#include "TFile.h"
+#include "TSystem.h"
+#include "TTreeIndex.h"
+
+#define HACK_FOR_ROOT
+
+#include "simpleStructs.h"
+
+using namespace std;
+
+void makeGpsTttTree(char *tttInName);
+void setupOutTree();
+
+
+GpsSubTime_t rawSubTime;
+
+
+TFile *theFile;
+char rootFileName[FILENAME_MAX];
+TTree *tttTree;
+
+int doneInit=0;
+
+int runNumber;
+UInt_t realTime;
+
+
+
+int main(int argc, char **argv) {
+  if(argc<3) {
+    std::cout << "Usage: " << basename(argv[0]) << " <ttt infile> outfile>" << std::endl;
+    return -1;
+  }
+  strncpy(rootFileName,argv[2],FILENAME_MAX);
+  makeGpsTttTree(argv[1]);
+  return 0;
+}
+
+
+
+void makeGpsTttTree(char *tttInName)
+{
+  setupOutTree();
+  {
+    std::cout << "Starting TTT" << endl;
+    ifstream GpsFile(tttInName);      
+    int numBytes=0;
+    char fileName[180];
+    int error=0;
+    int counter=0;
+    static int firstTtt=1;
+    while(GpsFile >> fileName) {
+      const char *subDir = gSystem->DirName(fileName);
+      const char *subSubDir = gSystem->DirName(subDir);
+      const char *typeDir = gSystem->DirName(subSubDir);
+      const char *unitDir= gSystem->DirName(typeDir);
+      const char *gpsDir = gSystem->DirName(unitDir);
+      const char *houseDir = gSystem->DirName(gpsDir);
+      const char *runDir = gSystem->DirName(houseDir);
+      const char *justRun = gSystem->BaseName(runDir);
+      //	std::cout << justRun << std::endl;
+      sscanf(justRun,"run%d",&runNumber);
+      
+      if(counter%100==0)
+	std::cout << fileName << std::endl;
+      counter++;
+      gzFile infile = gzopen (fileName, "rb");    
+      for(int i=0;i<1000;i++) {	
+	numBytes=gzread(infile,&rawSubTime,sizeof(GpsSubTime_t));
+	if(numBytes!=sizeof(GpsSubTime_t)) {
+	  error=1;
+	  break;
+	}
+	
+      }
+      gzclose(infile);
+      //	if(error) break;
+    }
+    tttTree->AutoSave();
+  }
+
+  theFile->Close();
+}
+
+
+void setupOutTree()
+{
+  theFile = new TFile(rootFileName,"RECREATE");
+  tttTree = new TTree("tttTree","Tree of GPS Sub Time Files");
+  tttTree->Branch("unixTimeGps",&rawSubTime.unixTime,"unixTimeGps/I");
+  tttTree->Branch("subTimeGps",&rawSubTime.subTime,"subTimeGps/I");
+  tttTree->Branch("fromAdu5",&rawSubTime.fromAdu5,"fromAdu5/i");
+}
---
+++ runAnita3TttFileMaker.sh
@@ -0,0 +1,50 @@
+#/bin/bash
+if [ "$1" = "" ]
+then
+   echo "usage: `basename $0` <run no>" 1>&2
+   exit 1
+fi
+
+RUN=$1
+#BASE_DIR=/unix/anita3/flight1415
+BASE_DIR=/Users/rjn/anita/anita3/data/
+RAW_RUN_DIR=${BASE_DIR}/raw/run${RUN}
+EVENT_BASE_DIR=${BASE_DIR}/root
+ROOT_RUN_DIR=${EVENT_BASE_DIR}/run${RUN}
+
+if [[ -d $ROOT_RUN_DIR ]]; then
+    echo "Output dir exists"
+elif [[ -d $RAW_RUN_DIR  ]]; then
+    mkdir ${ROOT_RUN_DIR}
+else
+    echo "$RAW_RUN_DIR doesn't exist what are we suppposed to rootify?"
+    exit 0;
+fi
+
+echo "Using $RAW_RUN_DIR"
+ls ${RAW_RUN_DIR}
+
+
+
+
+
+echo "Starting TTT  File"
+TTT_FILE_LIST=`mktemp`
+for file in ${RAW_RUN_DIR}/house/gps/all/ttt/*/*/ttt*.gz
+  do
+  if [[ -f $file ]]; then
+      echo $file >> ${TTT_FILE_LIST}
+  fi
+done
+
+if  test `cat ${TTT_FILE_LIST} | wc -l` -gt 0 ; then
+    TTT_ROOT_FILE=${ROOT_RUN_DIR}/tttFile${RUN}.root
+    ./makeGpsTttTree ${TTT_FILE_LIST} ${TTT_ROOT_FILE}
+    rm ${TTT_FILE_LIST}
+    echo "Done TTT  File"
+else
+    rm ${TTT_FILE_LIST}
+    echo "No TTT  Files"
+fi
+
+
