51ccccbfa890bf00b9dce21acd92c2d63d4d301c
LukeBatten <luke.batten.15@ucl.ac.uk>
2017-02-17 14:17:31 +0000

    Fixed small bug for determining if core folder exists

--- FolderScript.sh
+++ FolderScript.sh
@@ -10,13 +10,13 @@ fi
 ###
 
 RUN=$1
-BASE_DIR=/unix/anita4/flight2016/raw
+BASE_DIR=/unix/anita4/flight2016/raw/
 START_DIR=${BASE_DIR}/run${RUN}/start
 EVENT_DIR=${BASE_DIR}/run${RUN}/event
 HOUSE_DIR=${BASE_DIR}/run${RUN}/house
 
 if [ ! -d "$BASE_DIR" ]; then
-    echo "BASE ($TREE_MAKER_DIR) does not exist. Which raw file repositories are we supposed to check? Please include your BASE_DIR  file. Aborting."
+    echo "BASE_DIR ($BASE_DIR) does not exist. Which raw file repositories are we supposed to check? Please include your BASE_DIR name in this file. Aborting."
     exit 0;
 fi
 
